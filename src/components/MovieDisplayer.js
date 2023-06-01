76c1ec756f059df2b56ed6a59c1d5dfc0018e9b6
Ismael Freitas <ismael.freitas@sapo.pt>
2023-06-01 15:52:48 +0100

    Fix detail regaring page update when sorting changes

--- src/components/MovieDisplayer.js
+++ src/components/MovieDisplayer.js
@@ -17,6 +17,10 @@ function MovieDisplayer(props) {
     }, [])
 
     useEffect(() => {
+        setPage(1);
+    }, [sortOrder])
+
+    useEffect(() => {
 
         const newMoviesToDisplay = [...movies];
 
